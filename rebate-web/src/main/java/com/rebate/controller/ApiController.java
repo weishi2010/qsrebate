251b25dac0a9fc8a00cab6592e97e37c2ca25dc2
weishi <weishi@jd.com>
2017-11-01 13:22:53 +0800

    update

--- rebate-web/src/main/java/com/rebate/controller/ApiController.java
+++ rebate-web/src/main/java/com/rebate/controller/ApiController.java
@@ -1,32 +1,22 @@
 package com.rebate.controller;
 
 import com.rebate.common.util.EncodeUtils;
+import com.rebate.common.util.JsonUtil;
 import com.rebate.common.web.page.PaginatedArrayList;
 import com.rebate.controller.base.BaseController;
-import com.rebate.dao.UserInfoDao;
-import com.rebate.domain.UserInfo;
-import com.rebate.domain.en.*;
 import com.rebate.domain.property.JDProperty;
-import com.rebate.domain.query.ActivityQuery;
-import com.rebate.domain.query.ProductQuery;
 import com.rebate.domain.vo.ApiProductVo;
-import com.rebate.domain.vo.ProductVo;
-import com.rebate.service.activity.ActivityService;
-import com.rebate.service.activity.AdvertismentPositionService;
 import com.rebate.service.product.ProductService;
+import org.codehaus.jackson.map.util.JSONPObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -45,27 +35,24 @@ public class ApiController extends BaseController {
     private JDProperty jDProperty;
 
     @RequestMapping({"", "/", "/daxue/productList.json"})
-    public ResponseEntity<?> productList(HttpServletRequest request,String token, Integer page,Integer pageSize) {
+    @ResponseBody
+    public JSONPObject productList(HttpServletRequest request,String callback, Integer page,Integer pageSize) {
         if(null==page){
             page = 1;
         }
-        if(null==pageSize){
+        if(null==pageSize || pageSize>200){
             pageSize = 10;
         }
 
         String subUnionId = jDProperty.getApiSubUnionId();
 
         Map<String, Object> map = new HashMap<String, Object>();
-        if(validateToken(token,subUnionId)){
-            PaginatedArrayList<ApiProductVo> products = productService.findCouponProducts(subUnionId,page,pageSize);
-            map.put("products", products);
-            map.put("page", page);
-            map.put("totalItem", products.getTotalItem());
-            map.put("success",true);
-        }else{
-            map.put("success",false);
-        }
-        return new ResponseEntity<Map<String, ?>>(map, HttpStatus.OK);
+        PaginatedArrayList<ApiProductVo> products = productService.findCouponProducts(subUnionId, page, pageSize);
+        map.put("products", products);
+        map.put("page", page);
+        map.put("totalItem", products.getTotalItem());
+        map.put("success", true);
+        return  new JSONPObject(callback, map);
     }
 
 
