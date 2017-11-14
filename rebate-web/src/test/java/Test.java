c2c8b5a5dadd1d0a22e40f8b387efcc002abfc4d
weishi <wynsh2@163.com>
2017-11-14 23:34:44 +0800

    update

--- rebate-web/src/test/java/Test.java
+++ rebate-web/src/test/java/Test.java
@@ -6,6 +6,7 @@ import com.rebate.domain.ProductCoupon;
 import org.apache.commons.lang.StringUtils;
 import org.codehaus.jackson.type.TypeReference;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -49,54 +50,35 @@ public class Test {
         return conn.getHeaderField("Location");
     }
 
-    public static String get1(String oriUrl){
-        return HttpClientUtil.getFinalURL(oriUrl);
+    public static String getFinal(String oriUrl){
+        String urlStr = oriUrl;
+        try {
+            URL url = new URL(oriUrl);
+            HttpURLConnection conn=(HttpURLConnection)url.openConnection();
+            conn.getResponseCode();
+            urlStr=conn.getURL().toString();
+            conn.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return urlStr;
     }
 
+
     public static void main(String[] args) throws Exception {
-        System.out.println(DESUtil.decrypt("QIUSNHYAGHSNEIJAMYEFHQLRIHFADCHE", "ZoonL5QSpRTcUbETYotw==".replace(" ",""), "UTF-8"));
-//        String content = "[表情] 全站促销&神券导航一键直达，欢迎收藏转发。\n" +
-//                "\uD83C\uDF96 超市满199-100：http://t.cn/Rl2QBKM\n" +
-//                "\uD83C\uDF96 数码产品满千减百：http://t.cn/Rl28KhY\n" +
-//                "\uD83C\uDF96 电脑满3000减1000：http://t.cn/Rl28uah\n" +
-//                "\uD83C\uDF96 苹果8下单立减1111：http://t.cn/Rl2RvpP\n" +
-//                "\uD83C\uDF96 自营家电五折秒杀：http://t.cn/Rl2RUwv\n" +
-//                "\uD83C\uDF96 居家好物1.1元起：http://t.cn/Rl2RfIu\n" +
-//                "\uD83C\uDF96 建材家具厨具11.1元起：http://t.cn/Rl2RCPi\n" +
-//                "\uD83C\uDF96 天天1元秒生鲜http://t.cn/Rl2RHyM\n" +
-//                "\uD83C\uDF96 服装箱包1折秒杀：http://t.cn/Rl2RBDK\n" +
-//                "\uD83C\uDF96 运动服鞋五折秒：http://t.cn/Rl2EPiX\n" +
-//                "\uD83C\uDF96 童装童鞋三件7折：http://t.cn/Rl2EUsV\n" +
-//                "\uD83C\uDF96 图书每满100-50：http://t.cn/Rl2EMdh\n" +
-//                "\uD83C\uDF96 母婴爆款低至五折：http://t.cn/Rl2EppK\n" +
-//                "\uD83C\uDF96 中华老字号超值秒杀：http://t.cn/Rl2EQdR\n" +
-//                "\uD83C\uDF96 宠物商品买三免一：http://t.cn/Rl2EdO6\n" +
-//                "\uD83C\uDF96 医药保健超级秒：http://t.cn/Rl2nAc3\n" +
-//                "\uD83C\uDF96 自营车品满199-100：http://t.cn/Rl2ntvE\n" +
-//                "\uD83C\uDF96 全球购精品秒杀：http://t.cn/Rl2nXlk\n" +
-//                "\uD83C\uDF96 智能商品五折限量秒：http://t.cn/Rl2nlAE\n" +
-//                "[表情] 京东11.11权威发布！全站促销&神券导航一键直达，欢迎收藏转发。\n" +
-//                "\uD83C\uDF96  大牌钟表限时秒：http://t.cn/Rl2mJd1\n" +
-//                "\uD83C\uDF96 品牌特卖专区：http://t.cn/Rl2mjhm\n" +
-//                "\uD83C\uDF96 快速找货直接搜索：http://t.cn/Rl2mrwL\n" +
-//                "\uD83C\uDF96 京东秒杀频道实时秒杀：http://t.cn/Rl2u7CT\n" +
-//                "\uD83C\uDF96 11.11折扣活动大全：http://t.cn/Rl2utbc\n" +
-//                "\uD83C\uDF96 京东拼购，1元拼好货：http://t.cn/Rl2uOUw\n" +
-//                "\uD83C\uDFC5 点我天天领神券：http://t.cn/Rl2uu2s\n" +
-//                "\uD83C\uDFC5 礼品频道大促直达：http://t.cn/Rl23zaH\n" +
-//                "\uD83C\uDFC5 京东实时热卖榜，选择困难症患者点这里：http://t.cn/Rl23Vv7\n" +
-//                "\uD83C\uDFC5 满3000- 211全品券：http://t.cn/Rl21yUq\n" +
-//                "\uD83C\uDFC5 新plus会员领服饰600-150神券（可下黄金）：http://t.cn/Rl216A1\n" +
-//                "\uD83C\uDFC5  iPhone 800元神券：http://t.cn/Rl21Wvq\n" +
-//                "\uD83C\uDFC5 美赞臣好运抽奖：http://t.cn/Rl213b3";
-//        List<String> list = RegexUtils.getLinks(content);
-//        for (String link : list) {
-//            String clink = HttpClientUtil.convertJDPromotionUrl(link);
-//            if (StringUtils.isNotBlank(clink)) {
-//                content = content.replace(link, clink);
-//            }
-//        }
-//        System.out.println(content);
+        String url ="https://w.url.cn/s/AZeaQyw";
+        System.out.println(HttpClientUtil.convertJDPromotionUrl(url));
+
+
+        String content = "京东双11全部卖场的活动，全部卖场一键直达，卖场和商品下方都自带，优惠券的，记得爬楼看活动 ---------------------- 3分钟100亿 1.主会场，全品争锋，万千好货5折起！手机11.11元秒！ https://w.url.cn/s/ArUBHlD 2.家电会场，家电排行榜，价保30天！抢1000元神券！ https://w.url.cn/s/Af2VGYp 3.手机会场，潮流新phone爆，当燃京东！iPhone X现货抢！ https://w.url.cn/s/AmypdbX 4.什么值得买，京选会场！全场低至9.9元包邮！ https://w.url.cn/s/ARzilMB 5.数码会场，iPad Pro低至3688！ https://w.url.cn/s/AIE8ZwQ 6.居家生活，0点抢5折神券！跨店3件7折！自营199-100！https://w.url.cn/s/Aj3sSat 7.国际大牌品质购！满199减100！ https://w.url.cn/s/ASdWgGv 8.潮流服饰，5折神券嗨翻天，自营满199-100！ https://w.url.cn/s/AErXPrR 9.自营图书，满200-100！最高满300-180！ https://w.url.cn/s/Ak10JlZ --------------------- 【京东双十一狂欢11日领券汇总】 https://w.url.cn/s/ANfIMpC 京东双11领券大全各种免邮券，全品券速领数量有限 https://w.url.cn/s/AxR1JkI 超市全品188-100、699-200、499-150、299-90、99-30神券叠加满减 https://w.url.cn/s/AvR5OWJ 纸品清洁188-100神券叠加满减后神价 https://w.url.cn/s/ATEkmEU 食品饮料188-69、299-100 https://w.url.cn/s/Ae28gr9 母婴748-300、348-100、299-200神券 https://w.url.cn/s/AU5y9L9 全球购纸尿裤满900-300神券 https://w.url.cn/s/AgRdCUR 图书300-180 https://w.url.cn/s/AgN31gi 双11家电爆品排行榜 https://w.url.cn/s/AMToiIr 京东家电疯抢24小时 https://w.url.cn/s/A9Kv0zb 手机全品6000-1111、4980-700神券 https://w.url.cn/s/AmyOvBr 服饰五折神券可下黄金600-150服饰神券 https://w.url.cn/s/AZeaQyw 长按-发送给朋友分享给更多的人！";
+        List<String> list = RegexUtils.getLinks(content);
+        for (String link : list) {
+            String clink = HttpClientUtil.convertJDPromotionUrl(link);
+            if (StringUtils.isNotBlank(clink)) {
+                System.out.println(link+"----"+clink);
+            }
+        }
+        System.out.println(content);
     }
 
 }
